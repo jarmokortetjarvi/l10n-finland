271bf26b57b24a359f06e0c97d3aa5031a00ac4a
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-16 10:36:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Account Payment Terms Finnish
+==================
+
+Adds Finnish payment terms
+
+
+Payment terms
+========
+* IMMEDIATE
+* 7 days
+* 10 days
+* 14 days
+* 21 days
+* 30 days
+* 45 days
+* 60 days
+* 90 days
---
+++ __openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Account Payment Terms Finnish',
+    'category': 'Invoicing &amp; Payments',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'website': 'http://www.tawasta.fi',    
+    'depends': ['account_payment'],
+    'description': '''
+Account Payment Terms Finnish
+==================
+
+Adds Finnish payment terms
+
+
+Payment terms
+========
+* IMMEDIATE
+* 7 days
+* 10 days
+* 14 days
+* 21 days
+* 30 days
+* 45 days
+* 60 days
+* 90 days
+
+''',
+    'data': [
+
+    ],
+}
