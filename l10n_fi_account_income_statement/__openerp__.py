773dbe97060f9105a17c742f89c5cfbf25d1c7a6
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-13 13:17:51 +0300

    Add menu items

--- l10n_fi_account_income_statement/__openerp__.py
+++ l10n_fi_account_income_statement/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish Account Income Statement',
     'summary': 'Module summary',
-    'version': '8.0.0.7.0',
+    'version': '8.0.0.8.0',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -34,6 +34,8 @@
     ],
     'data': [
         'views/account_financial_report.xml',
+        'views/account_financial_report_menu.xml',
+        
         'data/init.xml',
         'security/account_financial_report_security.xml',
     ],
