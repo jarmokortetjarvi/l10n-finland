67542b291c4294db8b0f1255a652aa49bd068c6b
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-14 15:02:03 +0300

    Remove obsolete module summary

--- l10n_fi_account_income_statement/__openerp__.py
+++ l10n_fi_account_income_statement/__openerp__.py
@@ -21,8 +21,7 @@
 
 {
     'name': 'Finnish Account Income Statement',
-    'summary': 'Module summary',
-    'version': '8.0.0.8.3',
+    'version': '8.0.0.8.4',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
