e29fcf8bd5e4d3d218d582aef44b656fe68c7835
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-14 15:19:33 +0300

    Update menuitems

--- l10n_fi_account_ledger/__openerp__.py
+++ l10n_fi_account_ledger/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Account Ledger',
-    'version': '8.0.0.1.1',
+    'version': '8.0.0.1.2',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
