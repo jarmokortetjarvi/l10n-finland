ac03388e2ac6b68dfe5da2e31fe4549111dc5aef
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-14 15:04:47 +0300

    Add security groups

--- l10n_fi_account_income_statement/__openerp__.py
+++ l10n_fi_account_income_statement/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Account Income Statement',
-    'version': '8.0.0.8.4',
+    'version': '8.0.0.8.5',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
