075a6104029f555524274f7f7ccc04a0bdd0e154
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-29 12:31:23 +0200

    Add translations

--- l10n_fi_account_income_statement/__openerp__.py
+++ l10n_fi_account_income_statement/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Account Income Statement',
-    'version': '8.0.0.10.0',
+    'version': '8.0.0.10.1',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
