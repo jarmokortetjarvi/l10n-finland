262648ee7f88d60f8e18180bb4ff4ea2e9ae096d
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-11-17 18:01:51 +0200

    Improve industry fields: hide if partner isn't a company

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish Industry Classification',
     'category': 'CRM',
-    'version': '8.0.0.2.13',
+    'version': '8.0.0.2.14',
     'author': '''
 Oy Tawasta OS Technologies Ltd.,
 Vizucom Oy
