9d5dfe2740b13ee5039f440925b9ea3b7edbf6a1
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-12-15 09:28:28 +0200

    Remove an extra string from xml data

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Administrative Products',
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.2.1',
     'category': 'Localization',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
