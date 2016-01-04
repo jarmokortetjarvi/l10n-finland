d943d4365b07804153d2b421f5f2991f237743ee
Jarmo Kortetjärvi <jarmo@futurable.fi>
2016-01-04 16:16:30 +0200

    Add tax and account data

--- models/product_template.py
+++ models/product_template.py
@@ -47,6 +47,7 @@ class ProductTemplate(models.Model):
         taxes = dict()
 
         taxes['tax_purchase_0'] = 'Osto ALV 0%'
+        taxes['tax_purchase_10'] = 'Osto ALV 10%'
         taxes['tax_purchase_14'] = 'Osto ALV 14%'
         taxes['tax_purchase_24'] = 'Osto ALV 24%'
 
@@ -84,6 +85,185 @@ class ProductTemplate(models.Model):
         else:
             return False
 
+        # Kirjallisuus
         if external_id == 'l10n_fi_product_authority.product_template_ammattikirjallisuus':
             product.property_account_expense = self._search_account('8460', product.company_id)
             product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # ATK
+        elif external_id == 'l10n_fi_product_authority.product_template_atk_tarvikkeiden_pienhankinnat':
+            product.property_account_expense = self._search_account('7690', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Auton huolto ja tarvikkeet
+        elif external_id == 'l10n_fi_product_authority.product_template_auton_huolto_ja_korjaus':
+            product.property_account_expense = self._search_account('7540', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_autotarvikkeet_24':
+            product.property_account_expense = self._search_account('7550', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Edustus
+        elif external_id == 'l10n_fi_product_authority.product_template_edustuskulut':
+            product.property_account_expense = self._search_account('7994', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_edustuslahjat':
+            product.property_account_expense = self._search_account('7964', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Internet
+        elif external_id == 'l10n_fi_product_authority.product_template_internetkulut_24':
+            product.property_account_expense = self._search_account('8530', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Kahvitarvikkeet
+        elif external_id == 'l10n_fi_product_authority.product_template_kahvitarvikkeet_14':
+            product.property_account_expense = self._search_account('7111', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_14', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_kahvitarvikkeet_24':
+            product.property_account_expense = self._search_account('7110', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Kalusto ja kalusteet
+        elif external_id == 'l10n_fi_product_authority.product_template_kalusto_24':
+            product.property_account_expense = self._search_account('1170', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_kaytetyt_kalusteet_0':
+            product.property_account_expense = self._search_account('1201', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Kilometrikorvaukset
+        elif external_id == 'l10n_fi_product_authority.product_template_kilometrikorvaus_2015':
+            product.property_account_expense = self._search_account('7874', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_kilometrikorvauksen_korotus_1':
+            product.property_account_expense = self._search_account('7874', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_kilometrikorvauksen_korotus_perakarry':
+            product.property_account_expense = self._search_account('7874', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Päivärahat
+        elif external_id == 'l10n_fi_product_authority.product_template_kotimaan_osapaivaraha_2015':
+            product.property_account_expense = self._search_account('7884', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_kotimaan_paivaraha_2015':
+            product.property_account_expense = self._search_account('7884', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Lahjat hlökunnalle
+        elif external_id == 'l10n_fi_product_authority.product_template_lahja_henkilokunnalle':
+            product.property_account_expense = self._search_account('7160', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Mainostarvikkeet
+        elif external_id == 'l10n_fi_product_authority.product_template_mainostarvikkeet':
+            product.property_account_expense = self._search_account('8120', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Marjoitus
+        elif external_id == 'l10n_fi_product_authority.product_template_majoitus_10':
+            product.property_account_expense = self._search_account('7822', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_10', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_majoitus_ulkomaat':
+            product.property_account_expense = self._search_account('7824', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Matkaliput
+        elif external_id == 'l10n_fi_product_authority.product_template_matkaliput_0':
+            product.property_account_expense = self._search_account('7804', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_matkaliput_10':
+            product.property_account_expense = self._search_account('7802', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_10', product.company_id)
+
+        # Neuvottelukulut
+        elif external_id == 'l10n_fi_product_authority.product_template_neuvottelukulut_0':
+            product.property_account_expense = self._search_account('8654', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_neuvottelukulut_14':
+            product.property_account_expense = self._search_account('8651', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_14', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_neuvottelukulut_24':
+            product.property_account_expense = self._search_account('8650', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Paikoituskulut
+        elif external_id == 'l10n_fi_product_authority.product_template_paikoituskulut_0':
+            product.property_account_expense = self._search_account('7854', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_paikoituskulut_24':
+            product.property_account_expense = self._search_account('7850', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Polttoaine
+        elif external_id == 'l10n_fi_product_authority.product_template_polttoaine':
+            product.property_account_expense = self._search_account('7534', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Posti
+        elif external_id == 'l10n_fi_product_authority.product_template_postikulut_0':
+            product.property_account_expense = self._search_account('8544', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_postikulut_24':
+            product.property_account_expense = self._search_account('8540', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Puhelin
+        elif external_id == 'l10n_fi_product_authority.product_template_puhelinkulu_24':
+            product.property_account_expense = self._search_account('8500', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Sis. palaveri
+        elif external_id == 'l10n_fi_product_authority.product_template_sisaisen_palaverin_kulut':
+            product.property_account_expense = self._search_account('7010', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Taksi
+        elif external_id == 'l10n_fi_product_authority.product_template_taksikulut_0':
+            product.property_account_expense = self._search_account('7814', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_taksikulut_10':
+            product.property_account_expense = self._search_account('7812', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_10', product.company_id)
+
+        # Toimilupien rekisteröinnit
+        elif external_id == 'l10n_fi_product_authority.product_template_toimiluvat_rekisteroinnit_0':
+            product.property_account_expense = self._search_account('8444', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        # Toimistotarvikkeet
+        elif external_id == 'l10n_fi_product_authority.product_template_toimistotarvikkeet_24':
+            product.property_account_expense = self._search_account('8620', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_24', product.company_id)
+
+        # Ulkomaan päivärahat
+        elif external_id == 'l10n_fi_product_authority.product_template_ulkomaanpaivaraha_iso_britannia_2015':
+            product.property_account_expense = self._search_account('7894', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_ulkomaanpaivaraha_norja_2015':
+            product.property_account_expense = self._search_account('7894', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_ulkomaanpaivaraha_ruotsi_2015':
+            product.property_account_expense = self._search_account('7894', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
+
+        elif external_id == 'l10n_fi_product_authority.product_template_ulkomaanpaivaraha_viro_2015':
+            product.property_account_expense = self._search_account('7894', product.company_id)
+            product.supplier_taxes_id = self._search_tax('tax_purchase_0', product.company_id)
