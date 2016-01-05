ab1445d2d0984a42909a14943d14290733037b82
Jarmo Kortetjärvi <jarmo@futurable.fi>
2016-01-05 16:30:03 +0200

    Fix a typo

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Authority Products',
-    'version': '8.0.0.4.4',
+    'version': '8.0.0.4.5',
     'category': 'Localization',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
