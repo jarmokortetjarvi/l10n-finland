4ec325b7490c3e266716f2769297e914eb214e6f
Jarmo Kortetjärvi <jarmo@futurable.fi>
2016-01-05 16:58:39 +0200

    Add standard prices

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Authority Products',
-    'version': '8.0.0.4.5',
+    'version': '8.0.0.4.6',
     'category': 'Localization',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
