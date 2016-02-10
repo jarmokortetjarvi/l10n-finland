03b911cf93466a7026bac2491fa9be1e1c71f5ed
Jarmo Kortetjärvi <jarmo@futurable.fi>
2016-02-10 10:46:10 +0200

    Add a placeholder "analytic account" field

--- l10n_fi_account_income_statement/__openerp__.py
+++ l10n_fi_account_income_statement/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Jarmo Kortetjärvi
+#    Author: Oy Tawasta OS Technologies Ltd.
 #    Copyright 2015 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,7 @@
 
 {
     'name': 'Finnish Account Income Statement',
-    'version': '8.0.0.10.4',
+    'version': '8.0.0.10.5',
     'category': 'Account',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- l10n_fi_account_income_statement/models/accounting_report.py
+++ l10n_fi_account_income_statement/models/accounting_report.py
@@ -1,40 +1,55 @@
 # -*- coding: utf-8 -*-
 
 # 1. Standard library imports:
-#	import base64
 
-# 2. Known third party imports (One per line sorted and splitted in python stdlib):
-#	import lxml
+# 2. Known third party imports:
 
 # 3. Odoo imports (openerp):
 from openerp import api, fields, models
 
-# 4. Imports from Odoo modules (rarely, and only if necessary):
-#	from openerp.addons.website.models.website import slug
+# 4. Imports from Odoo modules:
 
 # 5. Local imports in the relative form:
-#	from . import utils
 
-# 6. Unknown third party imports (One per line sorted and splitted in python stdlib):
-#	_logger = logging.getLogger(__name__)
+# 6. Unknown third party imports:
 
 
 class AccountingReport(models.TransientModel):
-    _inherit='accounting.report'
-    
+
+    # 1. Private attributes
+    _inherit = 'accounting.report'
+
+    # 2. Fields declaration
+    analytic_account = fields.Many2one(
+        'account.analytic.account',
+        string="Analytic account",
+        domain=[('type', '=', 'normal')],
+    )
+
+    # 3. Default methods
     @api.model
     def default_get(self, fields):
-        ''' Get the correct financial report as default '''
+        # Get the correct financial report as default
         res = super(AccountingReport, self).default_get(fields)
 
         if 'report_code' in self._context:
             user =  self.env['res.users'].browse(self._uid)
-            
+
             report = self.env['account.financial.report'].search([
                 ('company', '=', user.company_id.id),
                 ('code', '=', self._context['report_code'])
             ])
-        
+
             res['account_report_id'] = report.id
 
         return res
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
