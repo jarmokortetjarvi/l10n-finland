2fa08b7adbff56a93fb8ddb4ec619f0ad5cab484
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-06-17 14:28:17 +0300

    Improve module structure

--- l10n_fi_crm_defaults/models/res_partner.py
+++ l10n_fi_crm_defaults/models/res_partner.py
@@ -6,13 +6,10 @@ class ResPartner(models.Model):
 
     _inherit = 'res.partner'
 
-    country_id = fields.Many2one(default=lambda self:
-                                 self.country_id.search([('code', '=', 'FI')]))
+    country_id = fields.Many2one(
+        default=lambda self:
+        self.country_id.search([('code', '=', 'FI')]))
     lang = fields.Selection(default='fi_FI')
-    # property_payment_term = fields.Many2one(default=lambda self:\
-    # self.env['account.payment.term'].search([('name', '=', '14 Days')]))
-    # property_supplier_payment_term = fields.Many2one(default=lambda self:\
-    # self.env['account.payment.term'].search([('name', '=', '14 Days')]))
 
     @api.model
     def default_get(self, fields):
